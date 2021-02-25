039f1646c3f9c8964c9a0be07fde9a17476c7b05
MatildaBjorken <matilda.bjork@hyperisland.se>
2021-02-25 15:12:59 +0100

    background image fixed

---
+++ src/components/nature.js
@@ -0,0 +1,17 @@
+import { Link } from "gatsby"
+import Logo from "../images/greenLogo.svg"
+import React from "react"
+import Farmer from '../images/farmer.png'
+
+const Nature = () =>{ 
+
+    return(
+
+        <div className='main'>
+        <div className='main-bg'  style={{ backgroundImage: `url(${Farmer})` }}>
+       
+        </div>
+        </div>
+      )
+}
+export default Nature
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,60 +6,76 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import Header from '../components/header'
+import Farmer from '../images/main-bg.jpg'
 
+
+import Test from '../images/farmer.png'
+import Nature from '../components/nature'
 
 const IndexPage = () => {
 
+  const nature = document.getElementById("nature")
+  const people= document.getElementById("nature")
+  const business = document.getElementById("nature")
+  const ricola = document.getElementById("nature")
+
+  const buttons = [nature, people, business, ricola]
+
+
+  function changeBackground(e){
+    e.target.style.background = { backgroundImage: `url(${Test})` }
+  }
+  
 
   return(
 
     <div className='main'>
-    <div className='main-bg'>
-    <Header/>
-    <div className='all-circles'>
+      <div className='main-bg'  style={{ backgroundImage: `url(${Farmer})` }}>
+        <Header/>
+        <div className='all-circles'>
 
-    <Link>
-    <div className='circle'>
-      <div className='circle-text'>
-        <span>N</span>
-        <br></br>
-        Nature
-        </div>
-    </div>
-    </Link>
+          <Link>
+            <div className='circle ' id='nature' onMouseOver={changeBackground}>
+              <div className='circle-text'>
+                <span>N</span>
+                <br></br>
+                Nature
+              </div>
+            </div>
+          </Link>
 
-    <Link>
-      <div className='circle'>
-        <div className='circle-text'>
-          <span>P</span>
-          <br></br>
-          People
-          </div>
-      </div>
-    </Link>
+          <Link>
+            <div className='circle' id='people'>
+              <div className='circle-text' >
+                <span>P</span>
+                <br></br>
+                People
+                </div>
+            </div>
+          </Link>
     
-    <Link>
-      <div className='circle'>
-        <div className='circle-text'>
-          <span>B</span>
-          <br></br>
-          Business
-        </div>
-      </div>
-    </Link>
+          <Link>
+            <div className='circle' id='business'>
+              <div className='circle-text'>
+                <span>B</span>
+                <br></br>
+                Business
+              </div>
+            </div>
+          </Link>
 
-    <Link>
-      <div className='circle'>
-        <div className='circle-text'>
-          <span>R</span>
-          <br></br>
-          Ricola
-        </div>
-      </div>
-    </Link>
+          <Link>
+            <div className='circle' id='ricola'>
+              <div className='circle-text'>
+                <span>R</span>
+                <br></br>
+                Ricola
+              </div>
+            </div>
+          </Link>
 
-    </div>  
-    </div>
+        </div>  
+      </div>
     </div>
   )
 }
